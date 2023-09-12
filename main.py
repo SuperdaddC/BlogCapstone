20eb85160be21c163a65ba3ebce8a92a5ee5ba44
SuperdaddC <mjcolyer@gmail.com>
2023-09-12 14:35:17 -0700

    Fixed edit form submission error. It was due to not having a post author in the form but haivng it in the return

--- main.py
+++ main.py
@@ -237,21 +237,23 @@ def add_new_post():
 @admin_only
 def edit_post(post_id):
     post = db.get_or_404(BlogPost, post_id)
+    print('this is the post', post.title)
     edit_form = CreatePostForm(
+        author=post.author,
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
         body=post.body
     )
-    if edit_form.validate_on_submit():
-        post.title = edit_form.title.data
-        post.subtitle = edit_form.subtitle.data
-        post.img_url = edit_form.img_url.data
-        post.author = current_user
-        post.body = edit_form.body.data
-        db.session.commit()
-        return redirect(url_for("show_post", post_id=post.id))
+    if request.method == "POST":
+        if edit_form.validate_on_submit():
+            post.title = edit_form.title.data
+            post.subtitle = edit_form.subtitle.data
+            post.img_url = edit_form.img_url.data
+            post.body = edit_form.body.data
+            post.author = edit_form.author.data
+            db.session.commit()
+            return redirect(url_for("show_post", post_id=post.id))
     return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
